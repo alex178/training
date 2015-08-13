30423d7400d7bda917429207a3cb2a5ec1d47519
alex178 <alex.info178@googlemail.com>
2015-08-13 21:53:27 +0200

    template und for schleife in template

--- microblog_part1/app/views.py
+++ microblog_part1/app/views.py
@@ -1,6 +1,11 @@
+# coding: utf8
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World!"
+	nameList = ["Lisa","Karl","Katja","Natalie","Stefan"]
+	user = {'nickname': 'Alex'}
+	return render_template('index.html',title='Home',user=user, this='this', iss='is', cool='cooool', nameList=nameList) #mehrere variablen können hier übergeben werden an das template
