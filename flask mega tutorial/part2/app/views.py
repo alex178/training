0017fa8ebe85c6cbeaaa77c4dcfbdb66bf539a9d
alex178 <alex.info178@googlemail.com>
2015-08-24 14:39:26 +0200

    templates

--- flask mega tutorial/part2/app/views.py
+++ flask mega tutorial/part2/app/views.py
@@ -1,7 +1,24 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
 from app import application
 
 #Ausgabe unter "/" oder "/index"
 @application.route('/')
 @application.route('/index')
 def index():
-	return 'Hello World!'
+	user = {'nickname': 'Flask-programmer'}
+	posts = [  
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Germany!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'Flask micro framework!' 
+        },
+        { 
+            'author': {'nickname': 'Karl'}, 
+            'body': 'Hello Osnabrueck!' 
+        }
+    ]
+	return render_template('index.html',title='Start',user=user,posts=posts)
